9bf94fa6afef836c54ff58fc6b1f9beee673a22d
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-10-19 20:43:06 +0200

    fix: analytics

--- index.php
+++ index.php
@@ -30,24 +30,16 @@ if (isset($_SESSION['isHTTPS'])) {
 <html lang="en">
 
 <head>
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-KMF849CF');
-    </script>
-    <!-- End Google Tag Manager -->
+   <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KRQD287WDE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KRQD287WDE');
+</script>
+<!-- end google tag -->
     <!-- mailchimp -->
     <script id="mcjs">
         ! function(c, h, i, m, p) {
@@ -110,9 +102,6 @@ if (isset($_SESSION['isHTTPS'])) {
 </head>
 
 <body class="lightest-background">
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMF849CF" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
     <div class="centeredflex">
         <div class="colorblocks">
             <?php if ($isHTTPS) : ?>
